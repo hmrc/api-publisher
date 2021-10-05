6f92c9c8a060bc5ee3cfa4c0ed479aef1a5686db
Neil Frow <675806+worthydolt@users.noreply.github.com>
2021-10-05 15:23:32 +0100

    Update Statuses.scala

--- app/uk/gov/hmrc/apipublisher/models/Statuses.scala
+++ app/uk/gov/hmrc/apipublisher/models/Statuses.scala
@@ -33,5 +33,4 @@ sealed trait ScopesDefinedResult
 
 case object ScopesDefinedOk extends ScopesDefinedResult
 
-//object ScopesNotDefined extends ScopesDefinedResult
 case class ScopesNotDefined(message: String) extends ScopesDefinedResult
