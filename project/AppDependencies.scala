466f4d6667c6bf1c307134a81d62336987403dd7
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2021-11-22 20:19:06 +0000

    API-5190: Fixed failing tests

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,23 +4,15 @@ import sbt._
 object AppDependencies {
   def apply(): Seq[ModuleID] = dependencies ++ testDependencies
   lazy val bootstrapVersion = "5.16.0"
-  lazy val jacksonVersion = "2.11.1"
 
   private lazy val dependencies = Seq(
     "uk.gov.hmrc"               %% "bootstrap-backend-play-28"    % bootstrapVersion,
-    "uk.gov.hmrc"               %% "raml-tools"                   % "1.18.0",
+    "uk.gov.hmrc"               %% "raml-tools"                   % "1.20.0",
     "uk.gov.hmrc"               %% "simple-reactivemongo"         % "8.0.0-play-28",
     "org.json"                  %  "json"                         % "20180130",
     "com.damnhandy"             %  "handy-uri-templates"          % "2.1.6",
-    "org.julienrf"              %% "play-json-derived-codecs"     % "6.0.0",
-    "com.typesafe.play"         %% "play-json"                    % "2.9.2",
-    "org.typelevel"             %% "cats-core"                    % "2.1.0",
-
-    "com.fasterxml.jackson.module"      %% "jackson-module-scala"           % jacksonVersion,
-    "com.fasterxml.jackson.core"        % "jackson-annotations"             % jacksonVersion,
-    "com.fasterxml.jackson.core"        % "jackson-databind"                % jacksonVersion,
-    "com.fasterxml.jackson.core"        % "jackson-core"                    % jacksonVersion,
-    "com.fasterxml.jackson.dataformat"  % "jackson-dataformat-yaml"         % jacksonVersion
+    "org.julienrf"              %% "play-json-derived-codecs"     % "9.0.0",
+    "org.typelevel"             %% "cats-core"                    % "2.1.0"
   )
 
   private lazy val testDependencies = Seq(
@@ -29,6 +21,7 @@ object AppDependencies {
     "org.scalaj"                %% "scalaj-http"                  % "2.4.1"                   % "test,it",
     "org.mockito"               %% "mockito-scala-scalatest"      % "1.7.1"                   % "test,it",
     "com.typesafe.play"         %% "play-test"                    % PlayVersion.current       % "test,it",
+    "com.typesafe.play"         %% "play-akka-http-server"        % "2.8.7"                   % "test,it",
     "com.github.tomakehurst"    %  "wiremock-jre8-standalone"     % "2.27.1"                  % "test,it"
   )
 }
