3ebe416938365540c14f77e7b693285b5351274a
Pete Slater <pslater@equalexperts.com>
2022-06-14 08:56:29 -0500

    APIS-5838 - Fixing jackson version (#80)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,6 +4,7 @@ import sbt._
 object AppDependencies {
   def apply(): Seq[ModuleID] = dependencies ++ testDependencies
   lazy val bootstrapVersion = "5.24.0"
+  lazy val jacksonVersion = "2.12.6"
 
   private lazy val dependencies = Seq(
     "uk.gov.hmrc"               %% "bootstrap-backend-play-28"    % bootstrapVersion,
@@ -13,15 +14,25 @@ object AppDependencies {
     "com.damnhandy"             %  "handy-uri-templates"          % "2.1.8",
     "org.julienrf"              %% "play-json-derived-codecs"     % "10.0.2",
     "org.typelevel"             %% "cats-core"                    % "2.6.1",
-    "io.swagger.parser.v3"      %  "swagger-parser"               % "2.0.31"
-
+    "io.swagger.parser.v3"      %  "swagger-parser"               % "2.0.33" excludeAll(
+      ExclusionRule("com.fasterxml.jackson.core", "jackson-databind"),
+      ExclusionRule("com.fasterxml.jackson.core", "jackson-core"),
+      ExclusionRule("com.fasterxml.jackson.core", "jackson-annotations"),
+      ExclusionRule("com.fasterxml.jackson.dataformat", "jackson-dataformat-yaml"),
+      ExclusionRule("com.fasterxml.jackson.datatype", "jackson-datatype-jsr310")
+    ),
+    "com.fasterxml.jackson.core" % "jackson-core" % jacksonVersion,
+    "com.fasterxml.jackson.core" % "jackson-databind" % jacksonVersion,
+    "com.fasterxml.jackson.core" % "jackson-annotations" % jacksonVersion,
+    "com.fasterxml.jackson.dataformat" % "jackson-dataformat-yaml" % jacksonVersion,
+    "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % jacksonVersion
   )
 
   private lazy val testDependencies = Seq(
-    "uk.gov.hmrc"               %% "bootstrap-test-play-28"       % bootstrapVersion         % "test,it",
-    "uk.gov.hmrc"               %% "reactivemongo-test"           % "5.0.0-play-28"          % "test,it",
+    "uk.gov.hmrc"               %% "bootstrap-test-play-28"       % bootstrapVersion          % "test,it",
+    "uk.gov.hmrc"               %% "reactivemongo-test"           % "5.0.0-play-28"           % "test,it",
     "org.scalaj"                %% "scalaj-http"                  % "2.4.2"                   % "test,it",
-    "org.mockito"               %% "mockito-scala-scalatest"      % "1.16.46"                   % "test,it",
+    "org.mockito"               %% "mockito-scala-scalatest"      % "1.16.46"                 % "test,it",
     "com.typesafe.play"         %% "play-test"                    % PlayVersion.current       % "test,it",
     "com.github.tomakehurst"    %  "wiremock-jre8-standalone"     % "2.31.0"                  % "test,it"
   )
