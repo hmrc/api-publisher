f75f6ba13b8d5cf530951ef2fc1a0205a28fa259
kskourtis1 <kskourtis1@users.noreply.github.com>
2025-05-30 14:53:48 +0100

    APIS-7574: Ensure the API Approval List is in the submitted orde (#150)

--- app/uk/gov/hmrc/apipublisher/repository/APIApprovalRepository.scala
+++ app/uk/gov/hmrc/apipublisher/repository/APIApprovalRepository.scala
@@ -26,7 +26,7 @@ import org.mongodb.scala.bson.Document
 import org.mongodb.scala.bson.conversions.Bson
 import org.mongodb.scala.model.Aggregates._
 import org.mongodb.scala.model.Filters.{equal, in}
-import org.mongodb.scala.model.Indexes.ascending
+import org.mongodb.scala.model.Indexes.{ascending, descending}
 import org.mongodb.scala.model.{IndexModel, IndexOptions}
 
 import play.api.Logging
@@ -110,7 +110,8 @@ class APIApprovalRepository @Inject() (mongo: MongoComponent, val clock: Clock)(
   private def runQuery(statusFilters: Bson) = {
     collection.aggregate[BsonValue](
       Seq(
-        filter(statusFilters)
+        filter(statusFilters),
+        sort(descending("createdOn"))
       )
     ).map(Codecs.fromBson[APIApproval])
       .toFuture()
